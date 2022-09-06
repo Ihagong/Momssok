64d4eeca75ca721e99d8739f84c2a860c3028025
unknown <ict.cspark@gmail.com>
2022-09-06 18:52:45 +0900

    [S07P22D203-93|FRONT] React 학습 - 기본

---
+++ FRONT/PCS/React/reactexam1/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  let name = "박찬석";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          안녕 리액트 {name}
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
