7586890cd7636d697ae14d30ec99023e0a57263e
pen2402 <pen2402@naver.com>
2022-09-07 16:01:26 +0900

    [S07P22D203-128|FRONT] recoil, recoil-persist 추가, 새로고침 시 작성된 일기 내용 유지

--- FRONT/src/Components/KeepDiaryComponent.js
+++ FRONT/src/Components/KeepDiaryComponent.js
@@ -1,12 +1,15 @@
-// import React, { useEffect } from 'react'
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { diaryContentState } from '../store/atoms'
+
 
 export function KeepDiaryComponent() {
-
+  const [diaryContent, setDiaryContent] = useRecoilState(diaryContentState)
+  
   return (
     <div>
       <h5>일기 작성</h5>
-      <textarea rows='5' cols='50' />
+      <textarea rows='5' cols='50' value={diaryContent} onChange={(e) => {setDiaryContent(e.target.value)}} />
     </div>
   );
 }
