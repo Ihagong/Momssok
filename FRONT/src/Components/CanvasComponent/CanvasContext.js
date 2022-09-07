0cd8ea6d7aa4bf2f1b02f8b5cec52efaeda7b90f
pen2402 <pen2402@naver.com>
2022-09-07 19:48:54 +0900

    [S07P22D203-128|FRONT] conflicts 해결

--- FRONT/src/Components/CanvasComponent/CanvasContext.js
+++ FRONT/src/Components/CanvasComponent/CanvasContext.js
@@ -1,9 +1,16 @@
-import React, { useContext, useRef, useState } from 'react'
+import React, { useContext, useRef, useState, useEffect } from 'react'
+import { Hands } from '@mediapipe/hands'
+import * as HANDS from '@mediapipe/hands'
+import * as cam from '@mediapipe/camera_utils'
+import Webcam from 'react-webcam'
+
 
 const CanvasContext = React.createContext()
 
 export const CanvasProvider = ({ children }) => {
   const [isDrawing, setIsDrawing] = useState(false)
+  const [gesture, setGesture] = useState('defaultGesture')
+  const [offset, setOffset] = useState({offsetX: 0, offsetY: 0})
   const canvasRef = useRef(null)
   const contextRef = useRef(null)
 
@@ -29,7 +36,19 @@ export const CanvasProvider = ({ children }) => {
     setIsDrawing(true)
   }
 
+  const startMotionDrawing = () => {
+    const { offsetX, offsetY } = offset
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setIsDrawing(true)
+  }
+
   const finishDrawing = () => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+  }
+
+  const finishMotionDrawing = () => {
     contextRef.current.closePath()
     setIsDrawing(false)
   }
@@ -39,6 +58,15 @@ export const CanvasProvider = ({ children }) => {
       return
     }
     const { offsetX, offsetY } = nativeEvent
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+  }
+
+  const motionDraw = () => {
+    if (!isDrawing) {
+      return
+    }
+    const { offsetX, offsetY } = offset
     contextRef.current.lineTo(offsetX, offsetY)
     contextRef.current.stroke()
   }
@@ -62,6 +90,91 @@ export const CanvasProvider = ({ children }) => {
     context.lineWidth = lineWidth
   }
 
+  const webcamRef = useRef(null)
+  const camCanvasRef = useRef(null)
+  const connect = window.drawConnectors
+  const drawLandmarks = window.drawLandmarks
+  var camera = null
+  function onResults(results) {
+    // const video = webcamRef.current.video
+    const videoWidth = webcamRef.current.video.videoWidth
+    const videoHeight = webcamRef.current.video.videoHeight
+
+    // Set canvas width
+    camCanvasRef.current.width = videoWidth
+    camCanvasRef.current.height = videoHeight
+
+    const canvasElement = camCanvasRef.current
+    const canvasCtx = canvasElement.getContext("2d")
+    
+    canvasCtx.save()
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height)
+    canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height)
+
+    const canvas = canvasRef.current
+
+    if (results.multiHandLandmarks) {
+      for (const landmarks of results.multiHandLandmarks) {
+        connect(canvasCtx, landmarks, HANDS.HAND_CONNECTIONS,
+                      {color: '#00FF00', lineWidth: 5})
+        drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2})
+        
+        if (landmarks[8].y < landmarks[7].y) {
+          setGesture('upGesture')
+          setOffset({offsetX: Math.ceil((1-landmarks[8].x)*(canvas.width/2)), offsetY: Math.ceil(landmarks[8].y*(canvas.height/2))})
+        }
+        else {
+          setGesture('downGesture')
+        }
+      }
+    }
+    canvasCtx.restore()
+  }
+  // }
+
+  // setInterval(())
+  useEffect(() => {
+
+    const hands = new Hands({locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+    }})
+
+    hands.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5
+    })
+
+    hands.onResults(onResults)
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      camera = new cam.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await hands.send({ image: webcamRef.current.video })
+        },
+        width: 640,
+        height: 480,
+      })
+      camera.start()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (gesture === 'upGesture') {
+      startMotionDrawing()
+    }
+    else {
+      finishMotionDrawing()
+    }
+  }, [gesture])
+
+  useEffect(() => {
+    motionDraw()
+  }, [offset])
   const saveCanvas = () => {
     const canvas = canvasRef.current
     const image = canvas.toDataURL()
@@ -86,7 +199,37 @@ export const CanvasProvider = ({ children }) => {
         draw,
       }}
     >
+    <Webcam
+      ref={webcamRef}
+      style={{
+        position: "absolute",
+        marginLeft: "auto",
+        marginRight: "auto",
+        left: 0,
+        right: 0,
+        textAlign: "center",
+        zindex: 9,
+        width: 128,
+        height: 96,
+      }}
+    />{" "}
+    <canvas
+      ref={camCanvasRef}
+      className="output_canvas"
+      style={{
+        position: "absolute",
+        marginLeft: "auto",
+        marginRight: "auto",
+        left: 0,
+        right: 0,
+        textAlign: "center",
+        zindex: 9,
+        width: 128,
+        height: 96,
+      }}
+    ></canvas>
       {children}
+      <p>fasdf{children.gesture}</p>
     </CanvasContext.Provider>
   )
 }
