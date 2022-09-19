aca729078a234ddf60884b7b7a85990663c4f953
pen2402 <pen2402@naver.com>
2022-09-20 01:25:58 +0900

    [S07P22D203-451|FRONT] 초기 프로필 페이지 및 프로필 페이지, 아이 프로필 컴포넌트 기본 틀 구현

--- FRONT/src/Pages/ProfilePage.js
+++ FRONT/src/Pages/ProfilePage.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { AddProfileButtonTag, AddProfileTextTag, ButtonTag3 } from '../Style/Components'
+import { useNavigate } from  'react-router-dom'
+import { ChildProfileComponent } from '../Components/ChildProfileComponent'
+
 
 function ProfilePage() {
+  const navigate = useNavigate()
+  const childrenList = [{ id: 0, name: '찬석이', birth: '2017.12', age: 6 }] // 아이 정보
+  
+  const handleClickCreateProfileButton = () => {
+    navigate('create')
+  }
+
   return (
     <>
+      {childrenList.length ?
+        <>
+          <h3>프로필을 선택해주세요.</h3>
+          <h4>우리 아이마다 맞춤으로 서비스 이용이 가능합니다.</h4>
+          {childrenList.map((info) => (
+            <ChildProfileComponent key={info.id} info={info} />
+          ))}
+          <ButtonTag3>아이 관리</ButtonTag3>
+          <ButtonTag3>로그아웃</ButtonTag3>
+        </>
+      :
+        <>
+          <h3>자녀 프로필 등록</h3>
+          <h4>우리 아이만을 위한 맘쏙 홈을 만나 보세요!</h4>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} onClick={handleClickCreateProfileButton}>
+            <AddProfileButtonTag rofileButtonTag><img src='icons/plus.svg' /></AddProfileButtonTag>
+            <AddProfileTextTag>프로필 등록</AddProfileTextTag>
+          </div>
+        </>
+      }
     </>
   );
 }
