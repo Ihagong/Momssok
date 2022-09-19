1b977ba0a50e38b90268cca4166fd08718aa861f
pen2402 <pen2402@naver.com>
2022-09-20 00:25:36 +0900

    [S07P22D203-449|FRONT] 회원 수정 페이지 기본 틀 구현

--- FRONT/src/Pages/EditAccountPage.js
+++ FRONT/src/Pages/EditAccountPage.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ButtonTag1, ButtonTag2, InputTag1 } from '../Style/Components'
+import { useParams } from  'react-router-dom'
+
 
 function EditAccountPage() {
+  const { accountId } = useParams()
+  
+  const [nickname, setNickname] = useState(accountId)
+  const [email, setEmail] = useState('')
+  const [currentPassword, setCurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [newPasswordCheck, setNewPasswordCheck] = useState('')
+ 
+  const handleClickAuthPassword = () => {
+  }
+
+  const handleClickEditAccount = () => {
+  }
+
   return (
     <>
+      <h2>맘쏙 회원수정</h2>
+      <p>닉네임과 비밀번호를</p>
+      <p>자유롭게 변경하실 수 있습니다.</p>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <InputTag1 type='text' placeholder='이름' value={nickname} onChange={(e) => {setNickname(e.target.value)}}/>
+        <InputTag1 type='email' placeholder='이메일' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+        <span>
+          <InputTag1 type='password' placeholder='현재 비밀번호' value={currentPassword} onChange={(e) => {setCurrentPassword(e.target.value)}}/>
+          <ButtonTag2 onClick={handleClickAuthPassword}>인증</ButtonTag2>
+        </span>
+        <InputTag1 type='password' placeholder='새 비밀번호' value={newPassword} onChange={(e) => {setNewPassword(e.target.value)}}/>
+        <p>8-16자의 영문 대소문자, 숫자 또는 특수문자 조합</p>
+        <InputTag1 type='password' placeholder='새 비밀번호 확인' value={newPasswordCheck} onChange={(e) => {setNewPasswordCheck(e.target.value)}}/>
+        { newPassword !== newPasswordCheck ?
+          <p>비밀번호가 다릅니다.</p>
+        : null }
+        <ButtonTag1 onClick={handleClickEditAccount}>회원 수정</ButtonTag1>
+      </div>
     </>
   );
 }
