5e9dcf3f254a1794cbd0ec5c079914e05e26d735
pen2402 <pen2402@naver.com>
2022-09-20 11:54:35 +0900

    [S07P22D203-455|FRONT] 아이 메인 페이지 기본 틀 구현, 메뉴 프로필 버튼 추가, 페이지 이동 기능 추가

--- FRONT/src/Pages/ChildMainPage.js
+++ FRONT/src/Pages/ChildMainPage.js
@@ -1,14 +1,37 @@
 import React, { useState } from 'react'
-import { LogoTag, ChildMenuTag, ChildMenuTabTag, ChildMenuSelectedTabTag, ChildSubMenuTag, ChildSubMenuButtonTag } from '../Style/Components'
+import { LogoTag, ChildMenuTag, ChildMenuTabTag, ChildMenuSelectedTabTag, ChildSubMenuTag, ChildSubMenuButtonTag, ChildProfileTag } from '../Style/Components'
+import { useNavigate } from  'react-router-dom'
 
 
 function ChildMainPage() {
   const [selectedTab, setSelctedTab] = useState(0)
 
+  const navigate = useNavigate()
+
+  const handleClickChildProfile = () => {
+    navigate('/profile')
+  }
+
+  const handleClickCreatePaintingButton = () => {
+    navigate('/painting/load')
+  }
+
+  const handleClickDiaryButton = () => {
+    navigate('/diary')
+  }
+
+  const handleClickCreateLetterButton = () => {
+    navigate('/letter/create')
+  }
+
+  const handleClickLetterButton = () => {
+    navigate('/letter')
+  }
+
   return (
     <>
       <ChildMenuTag>
-        <LogoTag src='icons/logo.svg'></LogoTag>
+        <LogoTag src='icons/logo.svg' />
         {selectedTab === 0 ? <ChildMenuSelectedTabTag><img src='icons/Dinosaur.svg' alt='그림' />그림</ChildMenuSelectedTabTag>
           : <ChildMenuTabTag onClick={() => setSelctedTab(0)}><img src='icons/Dinosaur.svg' alt='그림' />그림</ChildMenuTabTag>}
         {selectedTab === 1 ? <ChildMenuSelectedTabTag><img src='icons/Dinosaur.svg' alt='약속' />약속</ChildMenuSelectedTabTag>
@@ -17,15 +40,17 @@ function ChildMainPage() {
           : <ChildMenuTabTag onClick={() => setSelctedTab(2)}><img src='icons/Dinosaur.svg' alt='편지' />편지</ChildMenuTabTag>}
         {selectedTab === 3 ? <ChildMenuSelectedTabTag><img src='icons/Dinosaur.svg' alt='놀이' />놀이</ChildMenuSelectedTabTag>
           : <ChildMenuTabTag onClick={() => setSelctedTab(3)}><img src='icons/Dinosaur.svg' alt='놀이' />놀이</ChildMenuTabTag>}
+        <ChildProfileTag onClick={handleClickChildProfile}><img src='icons/profile_image.svg' />아이 이름</ChildProfileTag>
+        
       </ChildMenuTag>
       <ChildSubMenuTag>
         {selectedTab === 0 ?
           <>
-            <ChildSubMenuButtonTag>
+            <ChildSubMenuButtonTag onClick={handleClickCreatePaintingButton}>
               <LogoTag src='icons/draw.svg' />
               그림 그리기
             </ChildSubMenuButtonTag>
-            <ChildSubMenuButtonTag>
+            <ChildSubMenuButtonTag onClick={handleClickDiaryButton}>
               <LogoTag src='icons/draw.svg' />
               일기 쓰기
             </ChildSubMenuButtonTag>
@@ -33,18 +58,17 @@ function ChildMainPage() {
           : null }
         {selectedTab === 2 ?
           <>
-            <ChildSubMenuButtonTag>
+            <ChildSubMenuButtonTag onClick={handleClickCreateLetterButton}>
               <LogoTag src='icons/draw.svg' />
               편지 쓰기
             </ChildSubMenuButtonTag>
-            <ChildSubMenuButtonTag>
+            <ChildSubMenuButtonTag onClick={handleClickLetterButton}>
               <LogoTag src='icons/draw.svg' />
               편지 읽기
             </ChildSubMenuButtonTag>
           </>
           : null }
       </ChildSubMenuTag>
-
     </>
   );
 }
