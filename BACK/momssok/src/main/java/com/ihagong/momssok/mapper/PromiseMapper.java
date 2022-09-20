22463b808becbb216b90324c842c1aaf3e09b79d
test <test@example.com>
2022-09-21 00:30:13 +0900

    [S07P22D203-385|BACK] User, Letter, Promise 컨트롤러

--- BACK/momssok/src/main/java/com/ihagong/momssok/mapper/PromiseMapper.java
+++ BACK/momssok/src/main/java/com/ihagong/momssok/mapper/PromiseMapper.java
@@ -1,4 +1,12 @@
 package com.ihagong.momssok.mapper;
 
+import com.ihagong.momssok.model.dto.PromiseDBDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface PromiseMapper {
+    int checkPromise(String email_name);
+    int savePromise(PromiseDBDto dto);
+    PromiseDBDto selectPromise(String email_name);
+    int updatePromise(PromiseDBDto dto);
 }
--- BACK/momssok/src/main/java/com/ihagong/momssok/model/dto/LetterApiDto.java
+++ BACK/momssok/src/main/java/com/ihagong/momssok/model/dto/LetterApiDto.java
@@ -2,15 +2,16 @@ package com.ihagong.momssok.model.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.core.io.ByteArrayResource;
 
 import java.util.Date;
+
 @Getter
 @Setter
-public class LetterDto {
+public class LetterApiDto {
     private String send_from;
     private String send_to;
     private String title;
     private String content;
-    private String video_path;
     private Date date;
 }
