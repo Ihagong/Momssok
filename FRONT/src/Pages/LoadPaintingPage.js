a0f67c3704d6c200c16a9e4e9904b26e6a995fed
pen2402 <pen2402@naver.com>
2022-09-21 15:47:35 +0900

    [S07P22D203-457|FRONT] 그림 불러오기 페이지 기본 틀 구현 중, 그림 불러오기 기능 추가

--- FRONT/src/Pages/LoadPaintingPage.js
+++ FRONT/src/Pages/LoadPaintingPage.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useRecoilState } from 'recoil'
+import { loadedPaintingState } from '../store/atoms'
+
 
 function LoadPaintingPage() {
+  const [loadedPaintingSrc, setLoadedPaintingSrc] = useRecoilState(loadedPaintingState)
+
+  const navigate = useNavigate();
+
+  const handleChangeFile = (e) => {
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('painting', e.target.files[0])
+    const reader = new FileReader()
+    reader.readAsDataURL(e.target.files[0])
+    
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setLoadedPaintingSrc(reader.result)
+        resolve()
+        
+        navigate('/painting/create')
+      }
+    })
+  }
+
   return (
     <>
+      <input type='file' accept='image/*' onChange={(e) => handleChangeFile(e)} />
     </>
   );
 }
