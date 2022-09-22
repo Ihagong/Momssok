c1d0d4328a3f76b6b40731f9e50bff7c855c010b
pen2402 <pen2402@naver.com>
2022-09-23 01:42:27 +0900

    [S07P22D203-445|FRONT] 칭찬 도장 페이지 기본 틀 구현 - todo 기능 추가, 모달창 기능 추가

--- FRONT/src/store/atoms.js
+++ FRONT/src/store/atoms.js
@@ -15,3 +15,17 @@ export const loadedPaintingState = atom({
   default: '',
   effects_UNSTABLE: [persistAtom]
 })
+
+export const modalOpenState = atom({
+  key: 'modalOpenState',
+  default: false,
+  effects_UNSTABLE: [persistAtom]
+})
+
+export const todoListState = atom({
+  key: 'todoListState',
+  default: [{ id: 1, todo: 'todoList 레이아웃 잡기', done: false },
+    { id: 2, todo: 'todoList 기능 구현하기', done: false },
+    { id: 3, todo: 'todoListx css 스타일링 하기', done: false }],
+  effects_UNSTABLE: [persistAtom]
+})
