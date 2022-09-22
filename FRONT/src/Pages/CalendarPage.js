59b9ca17d1bb25b2b744dffc23577dbd61848387
pen2402 <pen2402@naver.com>
2022-09-22 10:55:42 +0900

    [S07P22D203-445|FRONT] 캘린더 페이지 - 캘린더 요일 추가, 이전달 및 다음달 이동 기능 추가

--- FRONT/src/Pages/CalendarPage.js
+++ FRONT/src/Pages/CalendarPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CalendarPage() {
   const date = new Date()
@@ -6,8 +6,11 @@ function CalendarPage() {
   const kstGap = 9*60*60*1000
   const today = new Date(utc + kstGap)
 
-  const currentYear = today.getFullYear()
-  const currentMonth = today.getMonth()+1
+  const [currentYear, setCurrentYear] = useState(today.getFullYear())
+  const [currentMonth, setCurrentMonth] = useState(today.getMonth()+1)
+
+  // const currentYear = today.getFullYear()
+  // const currentMonth = today.getMonth()+1
 
   // 이전 달 마지막 날 날짜와 요일
   const startDay = new Date(currentYear, currentMonth-1, 0)
@@ -18,6 +21,8 @@ function CalendarPage() {
   const endDay = new Date(currentYear, currentMonth, 0)
   const nextDate = endDay.getDate()
   const nextDay = endDay.getDay()
+
+  const Days = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
 
   const prevMonthDates = () => {
     const result = []
@@ -50,22 +55,53 @@ function CalendarPage() {
   }
 
   const templateRowsCount = () => {
-    let result = ''
+    let result = '1fr'
     for (let i = 0; i < Dates().length; i++) {
-      result += '1fr '
+      result += ' 1fr'
     }
     return result
+  }
+
+  const handleClickPrevMonth = () => {
+    if (currentMonth > 1) {
+      setCurrentMonth(currentMonth-1)
+    }
+    else if (currentYear > 1) {
+      setCurrentMonth(12)
+      setCurrentYear(currentYear-1)
+    }
+  }
+
+  const handleClickNextMonth = () => {
+    if (currentMonth < 12) {
+      setCurrentMonth(currentMonth+1)
+    }
+    else {
+      setCurrentMonth(1)
+      setCurrentYear(currentYear+1)
+    }
   }
 
   return (
     <>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <p>{currentMonth}월</p>
-      </div>
-      <div style={{ display: 'grid',
-        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr 1fr',
-        gridTemplateRows: {templateRowsCount} }}>
-          {prevMonthDates()} {currentMonthDates()} {nextMonthDates()}
+        <button onClick={handleClickPrevMonth}>이전달</button>
+        <div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <p>{currentYear}년 {currentMonth}월</p>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+          </div>
+          <div style={{ display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr 1fr',
+            gridTemplateRows: {templateRowsCount} }}>
+            {Days.map((day, index) => (
+              <div key={index}>{day}</div>
+            ))}
+            {prevMonthDates()} {currentMonthDates()} {nextMonthDates()}
+          </div>
+        </div>
+        <button onClick={handleClickNextMonth}>다음달</button>
       </div>
     </>
   );
