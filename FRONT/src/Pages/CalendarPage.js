a48805f3df0291eeca96ff133758290d8de8944e
pen2402 <pen2402@naver.com>
2022-09-22 10:25:53 +0900

    [S07P22D203-445|FRONT] 캘린더 페이지 - 캘린더 기본 기능 구현

--- FRONT/src/Pages/CalendarPage.js
+++ FRONT/src/Pages/CalendarPage.js
@@ -2,33 +2,29 @@ import React from 'react'
 
 function CalendarPage() {
   const date = new Date()
-  const utc = date.getTime() + (date.getTimezoneOffset() * 60 * 1000)
-  const kstGap = 9 * 60 * 60 * 1000
+  const utc = date.getTime() + (date.getTimezoneOffset()*60*1000)
+  const kstGap = 9*60*60*1000
   const today = new Date(utc + kstGap)
 
   const currentYear = today.getFullYear()
-  const currentMonth = today.getMonth() + 1
-
-  console.log(currentYear, currentMonth)
+  const currentMonth = today.getMonth()+1
 
   // 이전 달 마지막 날 날짜와 요일
   const startDay = new Date(currentYear, currentMonth-1, 0)
   const prevDate = startDay.getDate()
   const prevDay = startDay.getDay()
-  console.log(startDay, prevDate, prevDay)
 
   // 이달 마지막 날 날짜와 요일
   const endDay = new Date(currentYear, currentMonth, 0)
   const nextDate = endDay.getDate()
   const nextDay = endDay.getDay()
-  console.log(endDay, nextDate, nextDay)
-
-  console.log(prevDate, prevDay)
 
   const prevMonthDates = () => {
     const result = []
-    for (let i = prevDate - prevDay; i <= prevDate; i++) {
-      result.push(<div style={{ margin: '2px' }}>{i}</div>)
+    if (prevDay !== 6) {
+      for (let i = prevDate - prevDay; i <= prevDate; i++) {
+        result.push(<div key={i} style={{ margin: '2px' }}>{i}</div>)
+      }
     }
     return result
   }
@@ -36,24 +32,39 @@ function CalendarPage() {
   const currentMonthDates = () => {
     const result = []
     for (let i = 1; i <= nextDate; i++) {
-      result.push(<div style={{ margin: '2px' }}>{i}</div>)
+      result.push(<div key={i} style={{ margin: '2px' }}>{i}</div>)
     }
     return result
   }
 
   const nextMonthDates = () => {
     const result = []
-    for (let i = 1; i < (7 - nextDay == 7 ? 0 : 7 - nextDay); i++) {
-      result.push(<div style={{ margin: '2px' }}>{i}</div>)
+    for (let i = 1; i < 7-nextDay; i++) {
+      result.push(<div key={i} style={{ margin: '2px' }}>{i}</div>)
+    }
+    return result
+  }
+  
+  const Dates = () => {
+    return prevMonthDates() + currentMonthDates() + nextMonthDates()
+  }
+
+  const templateRowsCount = () => {
+    let result = ''
+    for (let i = 0; i < Dates().length; i++) {
+      result += '1fr '
     }
     return result
   }
 
   return (
     <>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <p>{currentMonth}월</p>
+      </div>
       <div style={{ display: 'grid',
         gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr 1fr',
-        gridTemplateRows: '1fr 1fr 1fr 1fr 1fr 1fr 1fr' }}>
+        gridTemplateRows: {templateRowsCount} }}>
           {prevMonthDates()} {currentMonthDates()} {nextMonthDates()}
       </div>
     </>
