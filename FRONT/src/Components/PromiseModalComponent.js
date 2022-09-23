4daf651d475a46da21725cd0a553654d5951996b
pen2402 <pen2402@naver.com>
2022-09-23 15:26:34 +0900

    [S07P22D203-445|FRONT] 칭찬 도장 페이지 기본 틀 구현 - todo 추가 기능 구현

--- FRONT/src/Components/PromiseModalComponent.js
+++ FRONT/src/Components/PromiseModalComponent.js
@@ -9,9 +9,18 @@ import { totalTodoListState, modalOpenState } from '../store/atoms'
 export function PromiseModalComponent({ promiseId }) {
   const [totalTodoList, setTotalTodoList] = useRecoilState(totalTodoListState)
   const [modalOpen, setModalOpen] = useRecoilState(modalOpenState)
+  const [isParent, setIsParent] = useState(true)
+  const [newTodo, setNewTodo] = useState('')
 
   const handleClickCloseModal = () => {
     setModalOpen(false)
+  }
+
+  const handleClickAddTodoButton = () => {
+    let newList = { ...totalTodoList }
+    newList[promiseId] = [...totalTodoList[promiseId], { id: newList[promiseId].length+1, todo: newTodo, done: false }]
+    console.log(newList)
+    setTotalTodoList(newList)
   }
 
   return (
@@ -23,6 +32,13 @@ export function PromiseModalComponent({ promiseId }) {
         )))
       :
         null }
+      { isParent ?
+        <div style={{ display: 'flex' }}>
+          <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+          <button onClick={handleClickAddTodoButton}>추가</button>
+        </div>
+        : null }
+
       <ChildButtonTag2 onClick={handleClickCloseModal}>닫기</ChildButtonTag2>
     </PromiseModalComponentTag>
   )
