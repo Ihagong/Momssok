ea0c29b45f0ab8025d650d2807a32ff37b39da23
pen2402 <pen2402@naver.com>
2022-09-23 12:05:51 +0900

    [S07P22D203-445|FRONT] 칭찬 도장 페이지 기본 틀 구현 - 개별 todo 기능 구현

--- FRONT/src/Components/PromiseModalComponent.js
+++ FRONT/src/Components/PromiseModalComponent.js
@@ -3,11 +3,11 @@ import { PromiseModalComponentTag } from '../Style/Components'
 import { ChildButtonTag2 } from '../Style/Components'
 import { PromiseTodoItemComponent } from './PromiseTodoItemComponent'
 import { useRecoilState } from 'recoil'
-import { todoListState, modalOpenState } from '../store/atoms'
+import { totalTodoListState, modalOpenState } from '../store/atoms'
 
 
-export function PromiseModalComponent() {
-  const [todoList, setTodoList] = useRecoilState(todoListState)
+export function PromiseModalComponent({ promiseId }) {
+  const [totalTodoList, setTotalTodoList] = useRecoilState(totalTodoListState)
   const [modalOpen, setModalOpen] = useRecoilState(modalOpenState)
 
   const handleClickCloseModal = () => {
@@ -16,10 +16,13 @@ export function PromiseModalComponent() {
 
   return (
     <PromiseModalComponentTag>
-      {todoList.map((todoItem) => (
-        <PromiseTodoItemComponent key={todoItem.id} todoItem={todoItem}>
-        </PromiseTodoItemComponent>
-      ))}
+      {totalTodoList[promiseId] ?
+        (totalTodoList[promiseId].map((todoItem) => (
+          <PromiseTodoItemComponent key={todoItem.id} todoItem={todoItem} promiseId={promiseId}>
+          </PromiseTodoItemComponent>
+        )))
+      :
+        null }
       <ChildButtonTag2 onClick={handleClickCloseModal}>닫기</ChildButtonTag2>
     </PromiseModalComponentTag>
   )
