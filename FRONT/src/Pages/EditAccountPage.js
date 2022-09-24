f31f29e631bfd438f1cd1810f88d047e30227e3d
pen2402 <pen2402@naver.com>
2022-09-24 22:16:56 +0900

    [S07P22D203-445|FRONT] 회원 수정 페이지 기본 틀 구현 - 회원 정보 조회 기능 추가

--- FRONT/src/Pages/EditAccountPage.js
+++ FRONT/src/Pages/EditAccountPage.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ButtonTag1, ButtonTag2, InputTag1 } from '../Style/Components'
-import { useParams } from  'react-router-dom'
+import axios from 'axios'
+import { useRecoilState } from 'recoil'
+import { logInTokenState, userInfoState } from '../store/atoms'
 
 
 function EditAccountPage() {
-  const { accountId } = useParams()
+  const [logInToken, setLogInToken] = useRecoilState(logInTokenState)
+  const [userInfo, setUserInfo] = useRecoilState(userInfoState)
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: '/api/user/detailUser',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': logInToken,
+      },
+    })
+    .then(response => {
+      if (response.data) {
+        console.log(response.data)
+        setUserInfo(response.data['user'])
+        console.log('회원 정보가 조회되었습니다.')
+      }
+    })
+    .catch(error => {
+      console.log(error.response.data)
+    })
+  }, [])
   
-  const [nickname, setNickname] = useState(accountId)
-  const [email, setEmail] = useState('')
+  const [nickname, setNickname] = useState(userInfo?.username)
+  const [email, setEmail] = useState(userInfo?.email)
   const [currentPassword, setCurrentPassword] = useState('')
   const [newPassword, setNewPassword] = useState('')
   const [newPasswordCheck, setNewPasswordCheck] = useState('')
