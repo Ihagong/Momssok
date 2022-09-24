c222bfeadc68ad3c96e2894d4965d6946f05502f
pen2402 <pen2402@naver.com>
2022-09-24 19:39:55 +0900

    [S07P22D203-449|FRONT] 회원가입 페이지 기본 틀 구현 - 이메일 인증 기능 추가, 회원가입 기능 추가

--- FRONT/src/Pages/SignUpPage.js
+++ FRONT/src/Pages/SignUpPage.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react'
 import { ButtonTag1, ButtonTag2, InputTag1 } from '../Style/Components'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 
 function SignUpPage() {
-  const [nickname, setNickname] = useState('')
+  const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordCheck, setPasswordCheck] = useState('')
   const [authNumber, setAuthNumber] = useState('')
+  const [token, setToken] = useState('')
 
-  const handleClickSignUp = () => {
-    if (password === passwordCheck) {
-    }
-  }
+  const navigate = useNavigate()
 
   const handleClickAuth = () => {
     if (email === '') {
@@ -32,16 +31,76 @@ function SignUpPage() {
       .then(response => {
         if (response.data) {
           console.log(response.data)
-        } else {
+          console.log('인증 메일을 보냈습니다.')
         }
       })
       .catch(error => {
-        console.log(error)
+        console.log(error.response.data)
       })
     }
   }
 
   const handleClickAuthCheck = () => {
+    if (authNumber === '') {
+      console.log('이메일을 입력해주세요.')
+    } else if (token !== '') {
+      axios({
+        method: 'post',
+        url: '/api/user/emailCertification',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: {
+          email,
+          code: authNumber,
+        }
+      })
+      .then(response => {
+        if (response.data) {
+          console.log(response.data)
+          console.log('인증되었습니다.')
+          setToken(response.data.token)
+        }
+      })
+      .catch(error => {
+        console.log(error.response.data)
+        setToken('')
+      })
+    }
+  }
+
+  const handleClickSignUp = () => {
+    if (username === '') {
+      console.log('이름을 입력해주세요.')
+    } else if (password === '' || password !== passwordCheck) {
+      console.log('비밀번호를 확인해주세요.')
+    } else if (token === '') {
+      console.log('이메일 인증을 진행해주세요.')
+    } else {
+      axios({
+        method: 'post',
+        url: '/api/user/signUp',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token
+        },
+        data: {
+          email,
+          password,
+          username,
+        }
+      })
+      .then(response => {
+        if (response.data) {
+          console.log(response.data)
+          console.log('회원가입에 성공했습니다.')
+          navigate('/login')
+        }
+      })
+      .catch(error => {
+        console.log(error.response.data)
+      })
+    }
   }
 
   return (
@@ -50,7 +109,7 @@ function SignUpPage() {
       <p>지금 회원가입 하신 후</p>
       <p>맘쏙의 다양한 서비스를 만나보세요.</p>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <InputTag1 type='text' placeholder='이름' value={nickname} onChange={(e) => {setNickname(e.target.value)}}/>
+        <InputTag1 type='text' placeholder='이름' value={username} onChange={(e) => {setUsername(e.target.value)}}/>
         <span>
           <InputTag1 type='email' placeholder='이메일' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
           <ButtonTag2 onClick={handleClickAuth}>인증</ButtonTag2>
