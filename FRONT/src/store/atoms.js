91481705eb468a98725901b859de7f56f1ba7f1a
pen2402 <pen2402@naver.com>
2022-09-24 20:56:35 +0900

    [S07P22D203-450|FRONT] 로그인 페이지 기본 틀 구현 - 로그인 기능 추가, 비밀번호 찾기 페이지 이동 추가

--- FRONT/src/store/atoms.js
+++ FRONT/src/store/atoms.js
@@ -4,6 +4,12 @@ import { recoilPersist } from 'recoil-persist'
 
 const { persistAtom } = recoilPersist()
 
+export const logInTokenState = atom({
+  key: 'logInTokenState',
+  default: '',
+  effects_UNSTABLE: [persistAtom]
+})
+
 export const diaryContentState = atom({
   key: 'diaryContentState',
   default: '',
