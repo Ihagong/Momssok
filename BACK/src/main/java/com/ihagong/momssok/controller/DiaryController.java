a915b24dc674509716d95931131cf7493a441a30
sr <hmr971127@gmail.com>
2022-09-25 22:22:55 +0900

    [S07P22D203-404|BACK]그림 조회 관련 api 구현
    Former-commit-id: 51c5ce2d8a8d33c0185b4ccf3b478fc4dfb5bb97 [formerly 2e06cbb6464c99cab5f278481ccdba1ec3ec5153]
    Former-commit-id: f1ab7dd7391e7a22f87fd6dfb2fda1ef47491c4c

--- BACK/src/main/java/com/ihagong/momssok/controller/DiaryController.java
+++ BACK/src/main/java/com/ihagong/momssok/controller/DiaryController.java
@@ -1,4 +1,67 @@
 package com.ihagong.momssok.controller;
 
+import com.ihagong.momssok.model.dto.DrawingDto;
+import com.ihagong.momssok.service.DiaryService;
+import com.ihagong.momssok.service.DrawingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@CrossOrigin("*")
 public class DiaryController {
+
+    private final String success = "SUCCESS";
+    private final String fail = "FAIL";
+    private final String error = "ERROR";
+    private final DiaryService diaryService;
+    private final DrawingService drawingService;
+
+    @GetMapping("/searchDrawing")
+    public Map<String, Object> lookupAllDrawing(@RequestParam String email_name){   //Param에 email과 name을 다 보내주는게 맞나?
+        Map<String, Object> result = new HashMap<>();
+        List<DrawingDto> drawingList = new ArrayList<>();
+
+        try {
+            drawingList = drawingService.lookupAllDrawing(email_name);
+
+            if(drawingList != null){
+                result.put("status", success);
+            }else{
+                result.put("status", fail);
+            }
+        } catch (Exception e) {
+            result.put("status", error);
+            result.put("message", e.toString());
+        }
+        result.put("data", drawingList);
+        return result;
+    }
+
+    @GetMapping("/detailDrawing/{drawing_id}")
+    public Map<String, Object> lookupDrawing(@PathVariable int drawing_id){
+        Map<String, Object> result = new HashMap<>();
+
+        DrawingDto drawing;
+        try {
+            drawing = drawingService.lookupDrawing(drawing_id);
+            if(drawing != null){
+                result.put("statue", success);
+                result.put("data", drawing);
+            }else{
+                result.put("status", fail);
+            }
+        } catch (Exception e) {
+            result.put("status", error);
+            result.put("message", e.toString());
+        }
+
+        return result;
+    }
 }
