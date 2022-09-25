a9b3fb8bf5a9dd4a0c024e75e768b03b3c64d0ca
sr <hmr971127@gmail.com>
2022-09-25 22:22:55 +0900

    [S07P22D203-404|BACK]그림 조회 관련 api 구현
    Former-commit-id: 3dc56dd1397f2bae4bc04e353d75c66b82d38466 [formerly cca4105211abdfdeffe5b82f1afc4452ecfe40cf]
    Former-commit-id: 705417927df7441b82edfa4088c9cc97800bb50d

---
+++ BACK/src/main/java/com/ihagong/momssok/service/DrawingServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ihagong.momssok.service;
+
+import com.ihagong.momssok.mapper.DrawingMapper;
+import com.ihagong.momssok.model.dto.DrawingDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DrawingServiceImpl implements DrawingService{
+
+    private final DrawingMapper drawingMapper;
+
+    @Override
+    public List<DrawingDto> lookupAllDrawing(String email_name) throws Exception {
+        return drawingMapper.lookupAllDrawing(email_name);
+    }
+
+    public DrawingDto lookupDrawing(int drawing_id) throws Exception {
+        return drawingMapper.lookupDrawing(drawing_id);
+    }
+}
