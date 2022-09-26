33a24dc60ba554af679ed685487dba3489619094
unknown <ict.cspark@gmail.com>
2022-09-27 08:59:41 +0900

    [S07P22D203-617|FRONT] 로그인 페이지 - 디자인 작업
    Former-commit-id: 00ab52de19b3bfd5e35976748e938d2174345e8f [formerly 972f94f399de3a9ca5ffac2d8c2bfcbbc75208f8]
    Former-commit-id: 062658acd3040bb54d78c118a654e4bce1e02ea3

--- FRONT/src/Pages/LogInPage.js
+++ FRONT/src/Pages/LogInPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { FindPasswordTag, SignUpTag, TextTag, ButtonTag1, InputTag1 } from '../Style/Components'
+import { LoginPageBody, GugiPink, JuaBrown, JuaBrownLight, FindPasswordTag, SignUpTag, TextTag, ButtonTag1, InputTag1 } from '../Style/Components'
 import { useNavigate } from  'react-router-dom'
 import { useAuthCallback } from '../Functions/useAuthCallback'
 
@@ -30,20 +30,28 @@ function LogInPage() {
   }
 
   return (
-    <>
-      <h2>환영합니다!</h2>
-      <p>소중한 우리 아이의 마음 속,</p>
-      <p>오늘은 어떤 그림이 그려졌을까요?</p>
-      <h4>우리 아이 마음 일기장</h4>
-      <h3>맘쏙</h3>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <InputTag1 type='email' placeholder='이메일' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
-        <InputTag1 type='password' placeholder='비밀번호' value={password} onChange={(e) => {setPassword(e.target.value)}}/>
-        <FindPasswordTag onClick={handleClickFindPassword}>비밀번호 찾기</FindPasswordTag>
-        <ButtonTag1 onClick={handleClickLogIn}>로그인</ButtonTag1>
-        <TextTag>아직 계정이 없으신가요? <SignUpTag onClick={handleClickSignUp}>회원가입</SignUpTag></TextTag>
-      </div>
-    </>
+    <LoginPageBody>
+      <section style={{marginRight: "60px", marginTop: "60px"}}>
+        <JuaBrown style={{fontSize: "80px"}}>환영합니다!</JuaBrown>
+        <JuaBrownLight style={{fontSize: "36px"}}>소중한 우리 아이의 마음 속,</JuaBrownLight>
+        <JuaBrownLight style={{fontSize: "36px"}}>오늘은 어떤 그림이 그려졌을까요?</JuaBrownLight>
+        <img style={{marginLeft: "40px", marginTop: "80px"}} src='/icons/bear.svg' />
+      </section>
+      <section style={{marginLeft: "40px", marginTop: "60px"}}>
+        <JuaBrown style={{fontSize: "48px"}}>우리 아이 마음 일기장</JuaBrown>
+        <div style={{marginBottom: "30px"}}>
+          <img src='/icons/heart.svg' />
+          <GugiPink style={{fontSize: "64px", marginLeft: "10px"}}>맘쏙</GugiPink>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <InputTag1 type='email' placeholder='이메일' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+          <InputTag1 type='password' placeholder='비밀번호' value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+          <FindPasswordTag onClick={handleClickFindPassword}>비밀번호 찾기</FindPasswordTag>
+          <ButtonTag1 onClick={handleClickLogIn} style={{marginTop: "50px"}}>로그인</ButtonTag1>
+          <TextTag style={{marginTop: "40px"}}>아직 계정이 없으신가요? <SignUpTag onClick={handleClickSignUp}>회원가입</SignUpTag></TextTag>
+        </div>
+      </section>
+    </LoginPageBody>
   );
 }
 
