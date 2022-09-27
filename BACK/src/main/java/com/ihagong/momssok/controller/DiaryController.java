c1af61565a27130e90159765bdd3fd4317dca68f
sr <hmr971127@gmail.com>
2022-09-28 02:14:41 +0900

    [S07P22D203-590|BACK]그림판 관련 api 구현
    Former-commit-id: 15f173f7e329f773fbdf5bd93a664a980fecfe4f [formerly d7e47748ecf97c253a934446ee10afc3ef644f15]
    Former-commit-id: 8832beca65332f168b8f26a1f16b60be68862979

--- BACK/src/main/java/com/ihagong/momssok/controller/DiaryController.java
+++ BACK/src/main/java/com/ihagong/momssok/controller/DiaryController.java
@@ -3,16 +3,11 @@ package com.ihagong.momssok.controller;
 import com.ihagong.momssok.model.dto.DrawingDto;
 import com.ihagong.momssok.service.DrawingService;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -33,7 +28,7 @@ public class DiaryController {
         List<DrawingDto> drawingList = new ArrayList<>();
 
         try {
-            String email= SecurityContextHolder.getContext().getAuthentication().getName();  //user의 eamil을 꺼낸다
+            String email= SecurityContextHolder.getContext().getAuthentication().getName();  //user의 email을 꺼낸다
             String email_name = email + "_" + name;
             drawingList = drawingService.lookupAllDrawing(email_name);
 
@@ -73,7 +68,7 @@ public class DiaryController {
 
     @PostMapping("/saveDrawing")
     public Map<String, Object> saveDrawing(  //프론트에서 base64를 받아 이미지로 변환시킨 뒤 db에 저장, ai 서버로 보내서 태그랑 같이 받아와 저장
-                                             @RequestParam String drawing){
+                                             @RequestBody ){  //base64 문자열, 아이 이름
 
         Map<String, Object> result = new HashMap<>();
 
@@ -81,44 +76,42 @@ public class DiaryController {
         System.out.println(base64);
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-        String fileName = simpleDateFormat.format(new Date());
+        String fileName = simpleDateFormat.format(new Date());  //파일명
+
+        //저장할 파일 경로
+        String path = "C:\\Users\\multicampus\\Desktop\\resources\\";
+
+        UUID uuid = UUID.randomUUID();  //파일명 중복 방지용 식별자
+        String filePath = path + fileName + "_" + uuid + ".png";
 
         try {
-            //저장할 파일 경로(실제 파일 생성 X)
-//            File filePath = new File(FileSystemView.getFileSystemView().getHomeDirectory() + "/resources/" + fileName);
-            String filePath = "C:\\Users\\multicampus\\Desktop\\resources\\20220927";
-            System.out.println(filePath);  //C:\Users\multicampus\Desktop\resources\20220927
 
             //base64를 이미지 파일로 변환하고 저장한다.
-//            byte[] imageBytes = Base64.getDecoder().decode(base64
-//                                        .replace('-', '+')
-//                                        .replace('_', '/'));
+            byte[] imageBytes = Base64.getDecoder().decode(base64
+                                        .replace('-', '+')
+                                        .replace('_', '/'));
 //            byte[] imageBytes = Base64Utils.decodeFromUrlSafeString(base64);
-            ByteArrayResource resource = new ByteArrayResource(javax.xml.bind.DatatypeConverter.parseBase64Binary(base64.toString()));
-            File targetFile = new File(filePath);
-            java.nio.file.Files.copy(
-                    resource.getInputStream(),
-                    targetFile.toPath(),
-                    StandardCopyOption.REPLACE_EXISTING);
-            IOUtils.closeQuietly(resource.getInputStream());
 
-            System.out.println(resource);
+            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+            fileOutputStream.write(imageBytes);
+            fileOutputStream.close();
 
-//            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
-//            fileOutputStream.write(resource);
-//            fileOutputStream.close();
-
-//            try {
-//                DrawingDto drawing =
-//                int res = drawingService.saveDrawing(painting)  //이미지 저장
-//                if (res == 1) {
-//                    result.put("status", success);
-//                    result.put("data", )
-//                }
-//            } catch (Exception e) {
-//                result.put("status", error);
-//                result.put("message", e.toString());
-//            }
+            try {
+                DrawingDto drawingDto = new DrawingDto();
+                String email= SecurityContextHolder.getContext().getAuthentication().getName();  //user의 email을 꺼낸다
+                String email_name = email + "_" + name;
+                drawingDto.setEmail_name(email_name);
+                drawingDto.setImage_path(filePath);
+                int res = drawingService.saveDrawing(drawingDto);  //이미지 저장
+                if (res == 1) {
+                    result.put("status", success);
+                }else{
+                    result.put("status", fail);
+                }
+            } catch (Exception e) {
+                result.put("status", error);
+                result.put("message", e.toString());
+            }
         } catch (Exception e) {
             result.put("status", error);
             result.put("message", e.toString());
@@ -126,16 +119,10 @@ public class DiaryController {
         return result;
     }
 
-//    @PostMapping("/saveDrawing")
-//    public static String decoder(String base64){
-//
-//        String data = base64.split(",")[1];  //base64 문자열
-//        byte[] imageBytes = Base64.getDecoder().decode(data);  //base64 문자열을 디코딩
-//        System.out.println(new String(imageBytes));  //디코딩
-////        FileUtils.writeByteArrayToFile(new File(target), imageBytes);
-//
-//        return "true";
-//    }
+    //AI 서버로 그림 분석 결과 요청할 때 그림보내기
+    //태그 - 시리얼라이즈로 처리
+
+
 
 //    @PutMapping("/updateDrawing")
 //    public Map<String, Object> updateDrawing(@RequestBody DrawingDto drawing){
@@ -147,16 +134,16 @@ public class DiaryController {
 //        }
 //    }
 
-//    @GetMapping("/getDrawing")  //이미지를 base64 형태로 인코딩하여 프론트에 보내기
-//    public String getBase64(@RequestParam int drawing_id){
-//
-//        String painting = drawingService.getDrawing(drawing_id);  //이미지를 가져온다
-//
-//        byte[] file = FileUtils.readFileToByteArray(new File(painting.getPath()));  //bytearray로 변환
-//        String base64 = Base64.getEncoder().encodeToString(file);  //base64로 인코딩
-//
-//        return base64;
-//    }
+    @GetMapping("/getDrawing")  //이미지를 base64 형태로 인코딩하여 프론트에 보내기
+    public String getBase64(@RequestParam int drawing_id){
+
+        String painting = drawingService.getDrawing(drawing_id);  //이미지를 가져온다
+
+        byte[] file = FileUtils.readFileToByteArray(new File(painting));  //bytearray로 변환
+        String base64 = Base64.getEncoder().encodeToString(file);  //base64로 인코딩
+
+        return base64;
+    }
 
     @DeleteMapping("/deleteDrawing")
     public Map<String, Object> deleteDrawing(@RequestBody int drawing_id){
@@ -164,6 +151,8 @@ public class DiaryController {
         Map<String, Object> result = new HashMap<>();
 
         try {
+            //이메일과 id를 같이 넣어주자
+
             String email = SecurityContextHolder.getContext().getAuthentication().getName();
             int res = drawingService.deleteDrawing(email);  //삭제하려는 사용자가 현재 사용자와 일치하면 삭제
             if(res == 1){
