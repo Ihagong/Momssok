18123521e1e6af545b79f1aba10eaa203b783c0e
sr <hmr971127@gmail.com>
2022-09-29 03:23:29 +0900

    [S07P22D203-594|BACK] 그림일기 조회 관련 api 구현
    Former-commit-id: 29e832faec55111126faa9a37abe1bfb7b106bd1 [formerly 84184e4cd2b2bffa94611e839fcdc2baa1585e03]
    Former-commit-id: 2e75201b84bc70be3cccab32b60cdc08e3207a6f

--- BACK/src/main/java/com/ihagong/momssok/controller/DiaryController.java
+++ BACK/src/main/java/com/ihagong/momssok/controller/DiaryController.java
@@ -1,9 +1,17 @@
 package com.ihagong.momssok.controller;
 
+import com.ihagong.momssok.model.dto.DiaryDayDto;
+import com.ihagong.momssok.model.dto.DiaryDto;
+import com.ihagong.momssok.model.dto.DrawingDto;
+import com.ihagong.momssok.service.DiaryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -14,6 +22,81 @@ public class DiaryController {
     private final String success = "SUCCESS";
     private final String fail = "FAIL";
     private final String error = "ERROR";
+
+    private final DiaryService diaryService;
+
+    @GetMapping("/searchDiaryCalendar")
+    public Map<String, Object> lookupDiaryCalender(  //'2022-09-21 05:29:35' 년월이 일치하는 일기의 emotion을 모두 가져온다
+            @RequestBody DiaryDayDto diary){  //date("2022-09"), name을 받는다
+        Map<String, Object> result = new HashMap<>();
+        List<DiaryDto> diaryList = new ArrayList<>();
+
+        try {
+            String email= SecurityContextHolder.getContext().getAuthentication().getName();
+            String name = diary.getName();
+            String email_name = email + "_" + name;
+            diary.setEmail_name(email_name);
+
+            diaryList = diaryService.lookupCalender(diary);
+
+            if(diaryList != null){
+                result.put("status", success);
+            }else{
+                result.put("status", fail);
+            }
+        } catch (Exception e) {
+            result.put("status", error);
+            result.put("message", e.toString());
+        }
+        result.put("data", diaryList);
+        return result;
+
+    }
+
+    @GetMapping("/searchDiaryGallery")
+    public Map<String, Object> lookupDiaryGallery(@RequestParam String name){  //최신순, 오래된 순 처리?
+        Map<String, Object> result = new HashMap<>();
+        List<DiaryDto> diaryList = new ArrayList<>();
+
+        try {
+            String email= SecurityContextHolder.getContext().getAuthentication().getName();
+            String email_name = email + "_" + name;
+
+            diaryList = diaryService.lookupGallery(email_name);
+
+            if(diaryList != null){
+                result.put("status", success);
+            }else{
+                result.put("status", fail);
+            }
+        } catch (Exception e) {
+            result.put("status", error);
+            result.put("message", e.toString());
+        }
+        result.put("data", diaryList);
+        return result;
+
+    }
+
+    @GetMapping("/searchDiary/{option}")
+    public Map<String, Object> searchDiary(@PathVariable String option){
+        Map<String, Object> result = new HashMap<>();
+        List<DiaryDto> diaryList = new ArrayList<>();
+
+        try {
+            diaryList = diaryService.searchDiary(option);
+            if(diaryList != null){
+                result.put("status", success);
+                result.put("data", diaryList);
+            }else{
+                result.put("status", fail);
+            }
+        } catch (Exception e) {
+            result.put("status", error);
+            result.put("message", e.toString());
+        }
+        return result;
+    }
 
 
 
--- BACK/src/main/java/com/ihagong/momssok/service/DiaryService.java
+++ BACK/src/main/java/com/ihagong/momssok/service/DiaryService.java
@@ -1,4 +1,12 @@
 package com.ihagong.momssok.service;
 
+import com.ihagong.momssok.model.dto.DiaryDayDto;
+import com.ihagong.momssok.model.dto.DiaryDto;
+
+import java.util.List;
+
 public interface DiaryService {
+
+    List<DiaryDto> lookupCalender(DiaryDayDto diary) throws Exception;
+    List<DiaryDto> lookupGallery(String email_name) throws Exception;
 }
