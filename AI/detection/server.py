efcda406acc5a502fef235d47f441f73b12be74e
daeun <asd5687@naver.com>
2022-09-29 11:33:14 +0900

    [S07P22D203-518|AI] response 이미지 배경변경
    Former-commit-id: 2c26f93380bf44fd6b3b6d7e68a68b53a1227d5a [formerly 9810754b9f65aaeee0407bdb6dd018835a7b5f82]
    Former-commit-id: d7aa5abb875fe2543d97c79649eacdc49aaf2b5b

--- AI/detection/server.py
+++ AI/detection/server.py
@@ -29,8 +29,18 @@ def from_image_to_bytes(img):
 @app.post("/tag")
 async def upload_photo(file: UploadFile):
     contents = await file.read()
-    with open("temp.jpg", "wb") as fp:
+    with open("temp.png", "wb") as fp:
         fp.write(contents)
+    im = Image.open('./temp.png')
+    x, y = im.size
+    px = im.load()
+
+    for i in range(0, x):
+        for j in range(0, y):
+            if px[i, j][-1] == 0:
+                px[i, j] = (255, 255, 255, 255)
+    im = im.convert('RGB')
+    im.save('temp.jpg')
     result = test.faster_rcnn("temp.jpg")
 
     return result
