b0177d41c661d82ac456d0c0ef5a08f74adacf36
ict-cspark <ict.cspark@gmail.com>
2022-09-30 11:23:37 +0900

    [S07P22D203-623|FRONT] 편지 페이지 - 오류 수정
    Former-commit-id: ac4aaacd4e638e17b5d8de531c778b8aa9f94a4d [formerly 1221b235d8444ab924661f40fc4e21bde032e66e]
    Former-commit-id: a19abec04f50c1b283916cc85f76d4e3b2c0fc55

--- FRONT/src/Pages/PromisePage.js
+++ FRONT/src/Pages/PromisePage.js
@@ -1,49 +1,81 @@
-import React, { useState } from 'react'
-import { ChildButtonTag1, ChildProfileTag, PromiseBoardTag, PromiseItemTag1, PromiseItemTag2 } from '../Style/Components'
-import { PromiseModalComponent } from '../Components/PromiseModalComponent'
+import React, { useEffect } from 'react'
+import { ProfileEditFooter, ProfileChildren, ProfileFooter, ProfileBlock, ProfileTitle, JuaOrange, JuaBrown, JuaBrownLight, AddProfileButtonTag, AddProfileTextTag, ButtonTag3 } from '../Style/Components'
+import { useNavigate } from  'react-router-dom'
+import { ChildProfileComponent } from '../Components/ChildProfileComponent'
 import { useRecoilState } from 'recoil'
-import { modalOpenState } from '../store/atoms'
+import { profileInfoState, profileState } from '../store/atoms'
+import { useAuthCallback } from '../Functions/useAuthCallback'
 
 
-function PromisePage() {
-  // const [modalOpen, setModalOpen] = useState(false);
-  const [modalOpen, setModalOpen] = useRecoilState(modalOpenState)
-  const [promiseId, setPromiseId] = useState(0)
+function ProfilePage() {
+  const [profileInfo, setProfileInfo] = useRecoilState(profileInfoState)
+  const [profileName, setProfileName] = useRecoilState(profileState)
+  const navigate = useNavigate()
+  
+  const { logOutCallback, profileInfoCallback } = useAuthCallback()
 
-  const handleClickPromiseItem = (id) => {
-    setPromiseId(id)
-    setModalOpen(true)
+  const handleClickCreateProfileButton = () => {
+    navigate('/profile/create')
   }
 
-  const promiseBoard = () => {
-    const result = []
-    for (let i = 0; i < 15; i++) {
-      if (i%2) {
-        result.push(<PromiseItemTag1 key={i} onClick={() => handleClickPromiseItem(i+1)}></PromiseItemTag1>)
-      } else {
-        result.push(<PromiseItemTag2 key={i} onClick={() => handleClickPromiseItem(i+1)}></PromiseItemTag2>)
-      }
+  const handleClickManageProfileButton = () => {
+    navigate('/profile/manage')
+  }
+
+  const handleClickChildProfile = (info) => {
+    setProfileName(info)
+    console.log(profileName)
+    navigate('/child')
+  }
+
+  useEffect(() => {
+    if (profileInfo.length === 0) {
+      profileInfoCallback()
     }
-    return result
-  }
+  }, [])
+
 
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-        { modalOpen ? <PromiseModalComponent promiseId={promiseId} /> : null }
-        <div style={{ display: 'flex' }}>
-          <ChildButtonTag1 style={{ width: '120px' }}>닫기</ChildButtonTag1>
-          <h3>OO이의 칭찬 도장</h3>
-          <ChildProfileTag ChildProfileTag><img src='/icons/boy.svg' />아이 이름</ChildProfileTag>
+      { profileInfo?.length !== 1 ?
+        <div>
+          <ProfileTitle>
+            <JuaBrown style={{fontSize: "55px"}}>프로필을 선택해주세요.</JuaBrown>
+            <JuaOrange style={{fontSize: "48px"}}>우리 아이마다 맞춤으로 서비스 이용이 가능합니다.</JuaOrange>
+          </ProfileTitle>
+          <ProfileBlock>
+            { profileInfo?.map((info, index) => {if (!info.is_parent) {
+                return <ChildProfileComponent key={index} info={info} handleClickChildProfile={handleClickChildProfile} />
+            }})}
+          </ProfileBlock>
+          <ProfileFooter>
+            <ButtonTag3 onClick={handleClickManageProfileButton} style={{marginLeft:"10px", marginRight:"10px"}}>아이 관리</ButtonTag3>
+            <ButtonTag3 onClick={logOutCallback} style={{marginLeft:"10px", marginRight:"10px"}}>로그아웃</ButtonTag3>
+          </ProfileFooter>
         </div>
-        <div style={{ display: 'flex' }}>
-          <PromiseBoardTag>
-            {promiseBoard()}
-          </PromiseBoardTag>
+      :
+        <div>
+          <ProfileTitle>
+            <JuaBrown style={{fontSize: "55px"}}>자녀 프로필 등록</JuaBrown>
+            <JuaOrange style={{fontSize: "48px"}}>우리 아이만을 위한 맘쏙 홈을 만나 보세요!</JuaOrange>
+          </ProfileTitle>
+          <ProfileBlock style={{ flexDirection: 'column'}} onClick={handleClickCreateProfileButton}>
+            <AddProfileButtonTag 
+              style={{ boxShadow: "0px 10px 20px rgba(255, 96, 41, 0.4)" }}>
+                <img src='/icons/plus.svg' />
+            </AddProfileButtonTag>
+            <AddProfileTextTag style={{marginTop: "5px"}}>프로필 등록</AddProfileTextTag>
+          </ProfileBlock>
+          <ProfileEditFooter>
+            <ButtonTag3 onClick={logOutCallback}>로그아웃</ButtonTag3>
+          </ProfileEditFooter>
+          <ProfileChildren>
+            <img src='/icons/children.svg'/>
+          </ProfileChildren>
         </div>
-      </div>
+      }
     </>
   );
 }
 
-export default PromisePage
+export default ProfilePage
