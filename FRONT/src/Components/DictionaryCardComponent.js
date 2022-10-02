24c061b5b7051d2b452b9c2a77a75058c20f655d
pen2402 <pen2402@naver.com>
2022-10-03 03:31:06 +0900

    [S07P22D203-612|FRONT] 동물도감 페이지 구현 - 설명 기능 추가
    Former-commit-id: e9624c7be8dedf42501d4d4dd99fc2e630002db1 [formerly 211cf09710e25e761aa67252d65b4933ac6f5a0f]
    Former-commit-id: bd2297c3989c2c6bb782a39ca7b0ce5d534aa91a

--- FRONT/src/Components/DictionaryCardComponent.js
+++ FRONT/src/Components/DictionaryCardComponent.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { DictionaryCardComponentTag } from '../Style/Components'
+import { DictionaryCardComponentTag, DictionaryOpenedCardComponentTag } from '../Style/Components'
 
 
-export function DictionaryCardComponent({ info, handleClickDictionaryCardComponent }) {
-  return (
-    <DictionaryCardComponentTag onClick={() => handleClickDictionaryCardComponent(info)}>
-      <img src={`/images/${info.name}/${info.name}.svg`}></img>
-      {info.name_ko}
-    </DictionaryCardComponentTag>
-  )
+export function DictionaryCardComponent({ info, handleClickDictionaryCardComponent, openDescription }) {
+  if (openDescription) {
+    return null
+  } else {
+    return (
+      <DictionaryCardComponentTag onClick={() => handleClickDictionaryCardComponent(info)}>
+        <img src={`/images/${info.name}/${info.name}.svg`}></img>
+        {info.name_ko}
+      </DictionaryCardComponentTag>
+    )
+  }
 }
