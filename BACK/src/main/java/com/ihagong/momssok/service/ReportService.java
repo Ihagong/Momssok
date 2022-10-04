65d5b1500afe4b0cf05c92c110f59fcf3dccb566
sr <hmr971127@gmail.com>
2022-10-04 13:08:12 +0900

    [S07P22D203-698|BACK] 백/프론트 연동 - 그림일기 기능
    Former-commit-id: 98e546921e90002266a9f9a12a652db448597ecf [formerly 97814503f0d48ce4300004402b06c99bfe36b78f]
    Former-commit-id: 293e93d9d0c02933ee26192c1e8189c3eabdfb55

---
+++ BACK/src/main/java/com/ihagong/momssok/service/ReportService.java
@@ -0,0 +1,12 @@
+package com.ihagong.momssok.service;
+
+import com.ihagong.momssok.model.dto.ReportDto;
+import com.ihagong.momssok.model.dto.ReportInputDto;
+
+import java.util.List;
+
+public interface ReportService {
+    ReportDto lookupDaily(ReportInputDto reportInput) throws Exception;
+    List<ReportDto> lookupWeekly(ReportInputDto reportInput) throws Exception;
+    List<ReportDto> lookupMontly(ReportInputDto reportInput) throws Exception;
+}
