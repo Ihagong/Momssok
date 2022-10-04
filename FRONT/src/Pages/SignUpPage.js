6b08da801f418de58b4e7d33fd0713fd4c74f0f3
pen2402 <pen2402@naver.com>
2022-10-04 15:27:03 +0900

    [S07P22D203-738|FRONT] 알림 모달창 추가, 회원가입 페이지 알림 모달창 적용
    Former-commit-id: c3dc7628e35bd9da78c167c8834fe49b05d4f9bf [formerly bbe9ef613c73c8c740a77797f1c2abebd1573427]
    Former-commit-id: 92586523c7e178413561609d72df5c1c17e5c0ef

--- FRONT/src/Pages/SignUpPage.js
+++ FRONT/src/Pages/SignUpPage.js
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom'
 import { useAuthCallback } from '../Functions/useAuthCallback'
 import { useRecoilState } from 'recoil'
 import { signUpTokenState } from '../store/atoms'
-
 import { GugiBrown, JuaBrown, JuaBrownLight,  LoginPageBody, ButtonTag1, ButtonTag2, InputTag1 } from '../Style/Components'
+import { AlertModalComponent } from '../Components/AlertModalComponent'
 
 
 
@@ -16,6 +16,8 @@ function SignUpPage() {
   const [password, setPassword] = useState('')
   const [passwordCheck, setPasswordCheck] = useState('')
   const [authNumber, setAuthNumber] = useState('')
+  const [modalOpen, setModalOpen] = useState(false)
+  const [modalContent, setModalContent] = useState('')
 
   const { authCheckCallback, authEmailCallback, signUpCallback } = useAuthCallback()
 
@@ -23,64 +25,72 @@ function SignUpPage() {
 
   const handleClickAuth = () => {
     if (email === '') {
-      console.log('이메일을 입력해주세요.')
+      setModalContent('이메일을 입력해주세요.')
+      setModalOpen(true)
     } else {
-      authEmailCallback(email)
+      authEmailCallback(email, setModalContent, setModalOpen)
     }
   }
 
   const handleClickAuthCheck = () => {
     if (authNumber === '') {
-      console.log('인증 번호를 입력해주세요.')
+      setModalContent('인증 번호를 입력해주세요.')
+      setModalOpen(true)
     } else {
-      authCheckCallback(email, authNumber)
+      authCheckCallback(email, authNumber, setModalContent, setModalOpen)
     }
   }
 
   const handleClickSignUp = () => {
     if (username === '') {
-      console.log('이름을 입력해주세요.')
+      setModalContent('이름을 입력해주세요.')
+      setModalOpen(true)
     } else if (password === '' || password !== passwordCheck) {
-      console.log('비밀번호를 확인해주세요.')
+      setModalContent('비밀번호를 확인해주세요.')
+      setModalOpen(true)
     } else if (signUpToken === '') {
-      console.log('이메일 인증을 진행해주세요.')
+      setModalContent('이메일 인증을 진행해주세요.')
+      setModalOpen(true)
     } else {
       signUpCallback(email, password, username)
     }
   }
 
   return (
-    <LoginPageBody>
-      <section style={{marginLeft: "30px", marginTop: "40px"}}>
-        <JuaBrown style={{fontSize: "80px"}}>
-          <GugiBrown style={{marginRight: "20px"}}>맘쏙</GugiBrown>
-          회원가입
-        </JuaBrown>
-        <JuaBrownLight style={{fontSize: "36px"}}>지금 회원가입 하신 후</JuaBrownLight>
-        <JuaBrownLight style={{fontSize: "36px"}}>맘쏙의 다양한 서비스를 만나보세요.</JuaBrownLight>
-        <img style={{marginLeft: "40px", marginTop: "80px"}} src='/icons/mom.svg' />
-      </section>
-      <section style={{marginLeft: "60px", marginTop: "40px"}}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <InputTag1 type='text' placeholder='이름' value={username} onChange={(e) => {setUsername(e.target.value)}}/>
-          <span>
-            <InputTag1 type='email' placeholder='이메일' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
-            <ButtonTag2 onClick={handleClickAuth} style={{marginLeft: "15px"}}>인증</ButtonTag2>
-          </span>
-          <span>
-            <InputTag1 type='text' placeholder='인증번호' value={authNumber} onChange={(e) => {setAuthNumber(e.target.value)}}/>
-            <ButtonTag2 onClick={handleClickAuthCheck} style={{marginLeft: "15px"}}>확인</ButtonTag2>
-          </span>
-          <InputTag1 type='password' placeholder='비밀번호' value={password} onChange={(e) => {setPassword(e.target.value)}}/>
-          <JuaBrownLight style={{fontSize: "18px", marginTop: "10px", marginLeft: "50px"}}>8-16자의 영문 대소문자, 숫자 또는 특수문자 조합</JuaBrownLight>
-          <InputTag1 type='password' placeholder='비밀번호 확인' value={passwordCheck} onChange={(e) => {setPasswordCheck(e.target.value)}}/>
-          { password !== passwordCheck ?
-            <JuaBrownLight style={{fontSize: "20px", marginTop: "10px", marginLeft: "110px", marginBottom: "-40px", color: "#ED0000"}}>비밀번호가 일치하지 않습니다.</JuaBrownLight>
-          : null }
-          <ButtonTag1 onClick={handleClickSignUp} style={{marginTop: "70px"}}>회원가입</ButtonTag1>
-        </div>
-      </section>
-    </LoginPageBody>
+    <>
+      { modalOpen ? <AlertModalComponent setModalOpen={setModalOpen} modalContent={modalContent} /> : null }
+      <LoginPageBody>
+        <section style={{marginLeft: "30px", marginTop: "40px"}}>
+          <JuaBrown style={{fontSize: "80px"}}>
+            <GugiBrown style={{marginRight: "20px"}}>맘쏙</GugiBrown>
+            회원가입
+          </JuaBrown>
+          <JuaBrownLight style={{fontSize: "36px"}}>지금 회원가입 하신 후</JuaBrownLight>
+          <JuaBrownLight style={{fontSize: "36px"}}>맘쏙의 다양한 서비스를 만나보세요.</JuaBrownLight>
+          <img style={{marginLeft: "40px", marginTop: "80px"}} src='/icons/mom.svg' />
+        </section>
+        <section style={{marginLeft: "60px", marginTop: "40px"}}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <InputTag1 type='text' placeholder='이름' value={username} onChange={(e) => {setUsername(e.target.value)}}/>
+            <span>
+              <InputTag1 type='email' placeholder='이메일' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+              <ButtonTag2 onClick={handleClickAuth} style={{marginLeft: "15px"}}>인증</ButtonTag2>
+            </span>
+            <span>
+              <InputTag1 type='text' placeholder='인증번호' value={authNumber} onChange={(e) => {setAuthNumber(e.target.value)}}/>
+              <ButtonTag2 onClick={handleClickAuthCheck} style={{marginLeft: "15px"}}>확인</ButtonTag2>
+            </span>
+            <InputTag1 type='password' placeholder='비밀번호' value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+            <JuaBrownLight style={{fontSize: "18px", marginTop: "10px", marginLeft: "50px"}}>8-16자의 영문 대소문자, 숫자 또는 특수문자 조합</JuaBrownLight>
+            <InputTag1 type='password' placeholder='비밀번호 확인' value={passwordCheck} onChange={(e) => {setPasswordCheck(e.target.value)}}/>
+            { password !== passwordCheck ?
+              <JuaBrownLight style={{fontSize: "20px", marginTop: "15px", marginLeft: "110px", marginBottom: "-40px", color: "#ED0000"}}>비밀번호가 일치하지 않습니다.</JuaBrownLight>
+            : null }
+            <ButtonTag1 onClick={handleClickSignUp} style={{marginTop: "70px"}}>회원가입</ButtonTag1>
+          </div>
+        </section>
+      </LoginPageBody>
+    </>
   );
 }
 
