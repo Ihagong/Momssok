07dd1ddf969a5e364420463f8ac3e06d17cf7c48
pen2402 <pen2402@naver.com>
2022-10-05 04:08:15 +0900

    [S07P22D203-745|FRONT] 진단 리포트 페이지 구현 - 주별 그래프 기본 틀 구현, 그래프 컴포넌트 분리
    Former-commit-id: 641a1de840247eb4e6684cb67b6c3f2e91d79e84 [formerly ca3380639e899d1d4693ed5cdf49f5ed348e04f2]
    Former-commit-id: b9f30cf1cc3c02b5fc121e11d171b847aeaa5dc0

--- FRONT/src/Pages/ReportPage.js
+++ FRONT/src/Pages/ReportPage.js
@@ -6,42 +6,11 @@ import { useRecoilState } from 'recoil'
 import { profileState, parentActiveState } from '../store/atoms'
 import { useReportCallback } from '../Functions/useReportCallback'
 
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+import { ReportDailyComponent } from '../Components/ReportDailyComponent'
+import { ReportWeeklyComponent } from '../Components/ReportWeeklyComponent'
+import { ReportMonthlyComponent } from '../Components/ReportMonthlyComponent'
+import { ReportWordCloudComponent } from '../Components/ReportWordCloudComponent'
 
-
-
-const data = [
-  {
-    subject: '행복',
-    A: 67,
-    B: 42,
-    fullMark: 100,
-  },
-  {
-    subject: '슬픔',
-    A: 16,
-    B: 23,
-    fullMark: 100,
-  },
-  {
-    subject: '놀람',
-    A: 42,
-    B: 30,
-    fullMark: 100,
-  },
-  {
-    subject: '불안',
-    A: 17,
-    B: 24,
-    fullMark: 100,
-  },
-  {
-    subject: '분노',
-    A: 12,
-    B: 21,
-    fullMark: 100,
-  },
-]
 
 function ReportPage() {
   const [profileInfo, setProfileInfo] = useRecoilState(profileState)
@@ -84,34 +53,10 @@ function ReportPage() {
           { selectedTab === 3 ? <ReportSelectedTabTag onClick={() => handleClickChangeTab(3)}>전체</ReportSelectedTabTag>
             : <ReportTabTag onClick={() => handleClickChangeTab(3)}>전체</ReportTabTag> }
         </div>
-        <EmotionReportTag>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '0 0 20px 50px' }}>
-            <RadarChart cx={200} cy={200} outerRadius={150} width={400} height={350} data={data} style={{ display: 'flex', justifyContent: 'center', margin: '0 0 0 30px' }} >
-              <PolarGrid stroke='var(--Brown-Stroke)' />
-              <PolarAngleAxis stroke='var(--Brown-Text)' dataKey='subject' />
-              <PolarRadiusAxis style={{ fontSize: '24px' }} stroke='var(--Brown-Stroke)' />
-              <Radar name='average' dataKey='B' stroke='#82ca9d' fill='#82ca9d' fillOpacity={0.3} />
-              <Radar name='date' dataKey='A' stroke='#8884d8' fill='#8884d8' fillOpacity={0.3} />
-            </RadarChart>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <div style={{ margin: '0 0 0 30px', width: '30px', height: '30px', backgroundColor: '#82ca9d99', border: '1px solid #82ca9d' }}></div>
-              <div style={{ margin: '0 16px 0 6px', fontSize: '24px' }}>평균</div>
-              <div style={{ width: '30px', height: '30px', backgroundColor: '#8884d899', border: '1px solid #8884d8' }}></div>
-              <div style={{ margin: '0 0 0 6px', fontSize: '24px' }}>오늘</div>
-            </div>
-          </div>
-          <div>
-            <div style={{ display: 'flex', justifyContent: 'end', margin: '30px 30px 30px 0', fontSize: '26px' }}>2020.10.05</div>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '0 80px 0 0px' }}>
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFE27D', width: '120px', height: '120px', borderRadius: '40px', margin: '0 0 30px 0' }}>
-                <img style={{ width: '80px' }} src={`icons/happy.svg`}/>
-              </div>
-              <div>오늘 아이의 가장 높은 감정은</div><br/>
-              <div style={{ fontSize: '40px' }}>"행복"입니다.</div><br/>
-              <div style={{ fontSize: '26px', color: 'var(--Brown-LightText', margin: '0 0 50px 0' }}>아이에게 고마움을 전해보세요.</div>
-            </div>
-          </div>
-        </EmotionReportTag>
+        { selectedTab === 0 ? <ReportDailyComponent /> : null }
+        { selectedTab === 1 ? <ReportWeeklyComponent /> : null }
+        { selectedTab === 2 ? <ReportMonthlyComponent /> : null }
+        { selectedTab === 3 ? <ReportWordCloudComponent /> : null }
       </div>
     </>
   )
