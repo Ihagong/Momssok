dd72ee4b68bba2a6e6ac6d5454f68a4bf1f09b61
김필재 <voyager872@gmail.com>
2022-10-05 13:34:14 +0900

    Update AI/emotion_detective/fastApi2.py
    Former-commit-id: d58603ed75578f306c980426202833a89cc7231c [formerly 4e6f0534efcd6eda4c4942c03001a3eef47b9fd7]
    Former-commit-id: eaedbcc3d8e130bdd95bedf07afe0dc505071a57

--- AI/emotion_detective/fastApi2.py
+++ AI/emotion_detective/fastApi2.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.post("/emotion")
 def read_item(item_id: str):
     A, B = predict(item_id)
-    A.A.tolist()
+    A.tolist()
     return {
         "angry": round(A[0],2),
         "Surprised": round(A[1],2),
